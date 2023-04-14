4a56bab132930a4b624afab51c619622c56cbf25
Rachit Patel <rlpatel1411@gmail.com>
2023-04-14 15:58:42 -0400

    Added 4.4 exercise

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,31 @@ const dummy = (blogs) => {
   : blogs
 }
 
+const totalLikes = (blogs) => {
+    const reducer =  (sum, blog) => {
+        return sum+blog.likes
+    }
+
+    return blogs.length === 1
+    ? blogs[0].likes
+    : blogs.reduce(reducer, 0)
+}
+
+const favouriteBlog = (blogs) => {
+    let mostLikes = 0
+    blogs.forEach(b => {
+        if(b.likes >= mostLikes) mostLikes = b.likes
+    })
+    let result  = []
+     result = blogs.filter(b => b.likes === mostLikes)
+     const {author: author,
+        likes: likes,
+        title: title} = result[0]
+    return {title, author, likes}
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog
 }
